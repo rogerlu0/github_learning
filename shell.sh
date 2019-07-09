e9b35bfc9c815bbe3ac903a3b6121f3acb9757eb
rogerlu0 <1600932949@qq.com>
2019-07-09 16:32:13 +0800

    Add a Shell Scipt

---
+++ shell.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# author: isluojie
+# Just for shell writing learning
+
+echo "Shell for sending paras.";
+para_1 = $0;
+echo "first paras is ${para_1}";
+echo "second paras is $1";
+echo "length of paras is $#";
